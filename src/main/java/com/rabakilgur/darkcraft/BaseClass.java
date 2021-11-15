6c64ba955902db5021cdebb7655b44f4b215d103
unknown <ivanknoll@web.de>
2021-11-15 19:25:54 +0100

    added banedBlocksList and blockPlacedEvent

--- src/main/java/com/rabakilgur/darkcraft/BaseClass.java
+++ src/main/java/com/rabakilgur/darkcraft/BaseClass.java
@@ -7,8 +7,11 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.CampfireBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.particle.Particle;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.particles.ParticleType;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
@@ -20,6 +23,7 @@ import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.WorldTickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -88,50 +92,82 @@ public class BaseClass{
 				});
 			}
 		}
+	}
 
-		@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
-		public static class EventHandlerPlayerLoggedIn{
-			@SubscribeEvent
-			public static void onPlayerLoggedIn(PlayerLoggedInEvent event){
-				PlayerEntity player = event.getPlayer();
-				World world = player.level;
-				CompoundNBT compountNBT = player.getPersistentData();
-				if(!compountNBT.getBoolean("PLAYERALREADYJOINED")){
-					BlockPos campFirePostiBlockPos = getCampFireBlockPos(world, player.blockPosition());
-					world.setBlock(campFirePostiBlockPos, Blocks.CAMPFIRE.defaultBlockState().setValue(CampfireBlock.LIT, true), 3);
-					compountNBT.putBoolean("PLAYERALREADYJOINED", true);
-				}
-			}
 
-			private static BlockPos getCampFireBlockPos(World world, BlockPos playerBlockPos){
-				BlockState air = Blocks.AIR.defaultBlockState();
-				BlockState water = Blocks.WATER.defaultBlockState();
-				List<BlockPos> possibleBlockPosList = new ArrayList<>();
-				for(int y = 0; y < 9; y++){
-					for(int z = 0; z < 9; z++){
-						for(int x = 0; x < 9; x++){
-							if(x == 4 && y == 4 && z ==4)
-								continue;
-							BlockPos blockPos = new BlockPos(playerBlockPos.getX()+4-x, playerBlockPos.getY()+4-y, playerBlockPos.getZ()+4-z);
-							if( (world.getBlockState(blockPos).getMaterial() == Material.TOP_SNOW
-							|| world.getBlockState(blockPos).getMaterial() == Material.REPLACEABLE_PLANT
-							|| world.getBlockState(blockPos).equals(air))
-							&& !world.getBlockState(blockPos.below()).equals(air)
-							&& !world.getBlockState(blockPos.below()).equals(water)
-							&& world.getBlockState(blockPos).getMaterial() != Material.LEAVES
-							&& world.getBlockState(blockPos).getMaterial() != Material.PLANT)
-								possibleBlockPosList.add(blockPos);
-						}
-					}
-				}
-				double random = Math.random();
-				try{
-					return possibleBlockPosList.get((int)(random * possibleBlockPosList.size()));
-				}
-				catch(Exception e){
-					return playerBlockPos.below();
-				}
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+	public static class EventHandlerPlayerLoggedIn{
+		@SubscribeEvent
+		public static void onPlayerLoggedIn(PlayerLoggedInEvent event){
+			PlayerEntity player = event.getPlayer();
+			World world = player.level;
+			CompoundNBT compountNBT = player.getPersistentData();
+			if(!compountNBT.getBoolean("PLAYERALREADYJOINED")){
+				BlockPos campFirePostiBlockPos = getCampFireBlockPos(world, player.blockPosition());
+				world.setBlock(campFirePostiBlockPos, Blocks.CAMPFIRE.defaultBlockState().setValue(CampfireBlock.LIT, true), 3);
+				compountNBT.putBoolean("PLAYERALREADYJOINED", true);
 			}
 		}
+
+		private static BlockPos getCampFireBlockPos(World world, BlockPos playerBlockPos){
+			BlockState air = Blocks.AIR.defaultBlockState();
+			BlockState water = Blocks.WATER.defaultBlockState();
+			List<BlockPos> possibleBlockPosList = new ArrayList<>();
+			for(int y = 0; y < 9; y++){
+				for(int z = 0; z < 9; z++){
+					for(int x = 0; x < 9; x++){
+						if(x == 4 && y == 4 && z ==4)
+							continue;
+						BlockPos blockPos = new BlockPos(playerBlockPos.getX()+4-x, playerBlockPos.getY()+4-y, playerBlockPos.getZ()+4-z);
+						if( (world.getBlockState(blockPos).getMaterial() == Material.TOP_SNOW
+						|| world.getBlockState(blockPos).getMaterial() == Material.REPLACEABLE_PLANT
+						|| world.getBlockState(blockPos).equals(air))
+						&& !world.getBlockState(blockPos.below()).equals(air)
+						&& !world.getBlockState(blockPos.below()).equals(water)
+						&& world.getBlockState(blockPos).getMaterial() != Material.LEAVES
+						&& world.getBlockState(blockPos).getMaterial() != Material.PLANT)
+							possibleBlockPosList.add(blockPos);
+					}
+				}
+			}
+			double random = Math.random();
+			try{
+				return possibleBlockPosList.get((int)(random * possibleBlockPosList.size()));
+			}
+			catch(Exception e){
+				return playerBlockPos.below();
+			}
+		}
+	}
+
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+	public static class EventHandlerBlockPlaced{
+		private static List<BlockState> banedBlocks = getListOfBanedBlocks();
+		@SubscribeEvent
+		public static void onBlockPlaced(BlockEvent.EntityPlaceEvent event){
+
+			if(banedBlocks.contains(event.getPlacedBlock())){
+				event.setCanceled(true);
+				event.getWorld().playSound(null, event.getPos(), SoundEvents.ENDERMAN_SCREAM, SoundCategory.HOSTILE, 10.0f, 0.5f);
+				//event.getWorld().addParticle(ParticleTypes.CRIMSON_SPORE, event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), 1D, 0D, 0D);
+			}
+
+
+		}
+
+		private static List<BlockState> getListOfBanedBlocks(){
+
+			List<BlockState> blockStateList = new ArrayList<>();
+			blockStateList.add(Blocks.GLOWSTONE.defaultBlockState());
+			blockStateList.add(Blocks.REDSTONE_LAMP.defaultBlockState());
+			blockStateList.add(Blocks.SEA_PICKLE.defaultBlockState());
+			blockStateList.add(Blocks.LANTERN.defaultBlockState());
+			blockStateList.add(Blocks.SOUL_LANTERN.defaultBlockState());
+			return blockStateList;
+		}
+
+
 	}
 }
