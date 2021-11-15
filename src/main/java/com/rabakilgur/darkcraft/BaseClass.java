a2f661a1c388b42e35a0f4f98666f9d91a171997
unknown <ivanknoll@web.de>
2021-11-15 18:41:36 +0100

    added campfire event handling

--- src/main/java/com/rabakilgur/darkcraft/BaseClass.java
+++ src/main/java/com/rabakilgur/darkcraft/BaseClass.java
@@ -1,10 +1,17 @@
 package com.rabakilgur.darkcraft;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.CampfireBlock;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.PlayerInteractionManager;
 import net.minecraft.util.DamageSource;
@@ -16,10 +23,12 @@ import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.LightType;
+import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.ServerTickEvent;
 import net.minecraftforge.event.TickEvent.WorldTickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -98,6 +107,66 @@ public class BaseClass{
 				});
 			}
 		}
+
+
+
+		@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class EventHandlerPlayerLoggedIn{
+        @SubscribeEvent
+        public static void onPlayerLoggedIn(PlayerLoggedInEvent event){
+
+
+
+            PlayerEntity player = event.getPlayer();
+            World world = player.level;
+            CompoundNBT compountNBT = player.getPersistentData();
+
+            if(!compountNBT.getBoolean("PLAYERALREADYJOINED")){
+                BlockPos campFirePostiBlockPos = getCampFireBlockPos(world, player.blockPosition());
+                world.setBlock(campFirePostiBlockPos, Blocks.CAMPFIRE.defaultBlockState().setValue(CampfireBlock.LIT, true), 3);
+                compountNBT.putBoolean("PLAYERALREADYJOINED", true);
+            }   
+        }
+
+        private static BlockPos getCampFireBlockPos(World world, BlockPos playerBlockPos){
+
+            BlockState air = Blocks.AIR.defaultBlockState();
+            BlockState water = Blocks.WATER.defaultBlockState();
+            List<BlockPos> possibleBlockPosList = new ArrayList<>();
+            
+            
+
+            for(int y = 0; y < 9; y++){
+                for(int z = 0; z < 9; z++){
+                    for(int x = 0; x < 9; x++){
+                        if(x == 4 && y == 4 && z ==4)
+                            continue;
+                        BlockPos blockPos = new BlockPos(playerBlockPos.getX()+4-x, playerBlockPos.getY()+4-y, playerBlockPos.getZ()+4-z);
+                        if( (world.getBlockState(blockPos).getMaterial() == Material.TOP_SNOW 
+                        || world.getBlockState(blockPos).getMaterial() == Material.REPLACEABLE_PLANT 
+                        || world.getBlockState(blockPos).equals(air)) 
+                        && !world.getBlockState(blockPos.below()).equals(air) 
+                        && !world.getBlockState(blockPos.below()).equals(water)
+                        && world.getBlockState(blockPos).getMaterial() != Material.LEAVES
+                        && world.getBlockState(blockPos).getMaterial() != Material.PLANT)
+                            possibleBlockPosList.add(blockPos);
+                    }
+                }
+            }
+
+            double random = Math.random();
+
+            try{
+                return possibleBlockPosList.get((int)(random * possibleBlockPosList.size())); 
+            }
+            catch(Exception e){
+                return playerBlockPos.below();
+            }
+        }
+
+    }
+
+
 		/*public static void onServerTick(WorldTickEvent event) {
 			if (event.phase.equals(Phase.START)) {
 				if (tickCounter < 200) {
